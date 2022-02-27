1d3b7e6d00bad029f333ae13032e0caf323c6303
Thev2Andy <ciprian.andrei002@gmail.com>
2022-02-27 16:06:58 +0200

    Added epilepsy warnings.

---
+++ Assets/Scripts/Epilepsy Warning/EpilepsyWarning.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EpilepsyWarning : MonoBehaviour
+{
+    public GameObject EpilepsyScreen;
+
+    private void Awake() {
+        if(PlayerPrefs.GetInt("EpilepsyWarning", 0) == 1) {
+            EpilepsyScreen.SetActive(false);
+            return;
+        }
+
+        PlayerPrefs.SetInt("EpilepsyWarning", 1);
+    }
+}
